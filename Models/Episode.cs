2574d56e815c6c9eedc13e91f1a7742867e67dcb
Devil7DK <dineshthangavel47@gmail.com>
2020-09-17 21:25:27 +0530

    Add model for episode object

---
+++ Models/Episode.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _9Anime_Downloader.Models
+{
+    public class Episode
+    {
+        public string Id { get; }
+        public string URL { get; }
+        public string Name { get; }
+
+        public Episode(string id, string url, string name)
+        {
+            this.Id = id;
+            this.URL = url;
+            this.Name = name;
+        }
+    }
+}
