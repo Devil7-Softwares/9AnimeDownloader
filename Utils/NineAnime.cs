396ec5c919262bf5a69c562da50911dbf73fb2e1
Devil7DK <dineshthangavel47@gmail.com>
2020-09-17 21:36:46 +0530

    Implemented method to fetch server and episode details

---
+++ Utils/NineAnime.cs
@@ -0,0 +1,63 @@
+﻿using _9Anime_Downloader.Models;
+using AngleSharp;
+using AngleSharp.Dom;
+using Newtonsoft.Json;
+using RestSharp;
+using System.Collections.Generic;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace _9Anime_Downloader.Utils
+{
+    public static class NineAnime
+    {
+        private static RestClient client;
+
+        public static void SetDomain(string domain)
+        {
+            client = new RestClient(domain);
+
+            ServicePointManager.ServerCertificateValidationCallback += (sender, certificate, chain, errors) => true;
+        }
+
+        public static async Task<Server[]> GetServers(string animeId)
+        {
+            return await Task.Run(async () =>
+            {
+                IConfiguration config = Configuration.Default;
+
+                //Create a new context for evaluating webpages with the given config
+                IBrowsingContext context = BrowsingContext.New(config);
+
+                List<Server> servers = new List<Server>();
+
+                RestRequest request = new RestRequest(APINodes.GET_EPISODES(animeId));
+                RestResponse response = (RestResponse)client.Execute(request);
+                if (response.IsSuccessful)
+                {
+                    HTMLJSONResponse jsonResponse = JsonConvert.DeserializeObject<HTMLJSONResponse>(response.Content);
+                    if (jsonResponse != null)
+                    {
+                        IDocument document = await context.OpenAsync(req => req.Content(jsonResponse.html));
+                        IHtmlCollection<IElement> serverElements = document.QuerySelectorAll(".widget.servers .widget-title .tabs .tab");
+
+                        foreach (IElement serverElement in serverElements)
+                        {
+                            Server server = new Server(int.Parse(serverElement.GetAttribute("data-name")), serverElement.TextContent);
+
+                            IHtmlCollection<IElement> episodeElements = document.QuerySelectorAll(string.Format(".widget.servers .widget-body .server[data-id=\"{0}\"] a", server.Id));
+                            foreach (IElement episodeElement in episodeElements)
+                            {
+                                server.Episodes.Add(new Episode(episodeElement.GetAttribute("data-id"), episodeElement.GetAttribute("href"), episodeElement.TextContent));
+                            }
+
+                            servers.Add(server);
+                        }
+                    }
+                }
+
+                return servers.ToArray();
+            });
+        }
+    }
+}
