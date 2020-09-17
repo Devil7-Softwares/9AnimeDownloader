e56fab959590689487e10bdf108e27e97723556c
Devil7DK <dineshthangavel47@gmail.com>
2020-09-17 21:25:39 +0530

    Add model for server object

---
+++ Models/Server.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _9Anime_Downloader.Models
+{
+    public class Server
+    {
+        public int Id { get; }
+        public string Name { get; }
+        public List<Episode> Episodes { get; }
+
+        public Server(int id, string name)
+        {
+            this.Id = id;
+            this.Name = name;
+            this.Episodes = new List<Episode>();
+        }
+    }
+}
