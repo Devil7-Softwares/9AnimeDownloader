3979107310560ddc38361aa9bfa482956c179624
Devil7DK <dineshthangavel47@gmail.com>
2020-09-17 21:33:05 +0530

    Implemented parsing domain & anime id from url

--- Program.cs
+++ Program.cs
@@ -1,12 +1,32 @@
-﻿using System;
+﻿using _9Anime_Downloader.Utils;
+using System;
+using System.Text.RegularExpressions;
 
 namespace _9Anime_Downloader
 {
     class Program
     {
+        private static string Domain = "";
+        private static string AnimeId = "";
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            while (true)
+            {
+                Console.Write("\nEnter URL 9Anime Website URL: ");
+                string tmpURL = Console.ReadLine();
+                Match urlMatch = Regex.Match(tmpURL, Patterns.REGEX_MAINURL);
+                if (urlMatch.Success)
+                {
+                    Domain = urlMatch.Groups["domain"].Value;
+                    AnimeId = urlMatch.Groups["animeId"].Value;
+                    break;
+                }
+                else
+                {
+                    Console.Error.WriteLine("ERROR: Invalid URL. Try again...");
+                }
+            }
         }
     }
 }
