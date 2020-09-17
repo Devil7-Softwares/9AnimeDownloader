3ab0df753f8cf1dbea151c599ecdad9f83d2c760
Devil7DK <dineshthangavel47@gmail.com>
2020-09-17 21:29:43 +0530

    Added regex pattern for 9Anime url matching

---
+++ Utils/Patterns.cs
@@ -0,0 +1,7 @@
+﻿namespace _9Anime_Downloader.Utils
+{
+    public static class Patterns
+    {
+        public const string REGEX_MAINURL = @"(?<domain>http[s]?:\/\/.*?\..*?\..*?\/)\watch\/.*(\.(?<animeId>.*?)\/).*";
+    }
+}
