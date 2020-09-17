adc87af8055816e609667197e786285865090ef1
Devil7DK <dineshthangavel47@gmail.com>
2020-09-17 21:26:03 +0530

    Add model for html json response object

---
+++ Models/HTMLJSONResponse.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _9Anime_Downloader.Models
+{
+    public class HTMLJSONResponse
+    {
+        public string html { get; set; }
+    }
+}
