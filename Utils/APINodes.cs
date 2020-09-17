a947f917de1dae4544538f484c6254b6a37b962c
Devil7DK <dineshthangavel47@gmail.com>
2020-09-17 21:35:05 +0530

    Add module for keeping rest api urls

---
+++ Utils/APINodes.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _9Anime_Downloader.Utils
+{
+    public static class APINodes
+    {
+        public static string GET_EPISODES(string animeId) => string.Format("/ajax/film/servers?id={0}&ts={1}", animeId, DateTimeOffset.UtcNow.ToUnixTimeSeconds());
+    }
+}
