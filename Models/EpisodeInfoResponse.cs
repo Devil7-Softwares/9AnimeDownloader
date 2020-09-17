1b2428d69ff737ee8da6ef0eb4121aba2fc93bd5
Devil7DK <dineshthangavel47@gmail.com>
2020-09-18 01:38:30 +0530

    Add model for episode info response

---
+++ Models/EpisodeInfoResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace _9Anime_Downloader.Models
+{
+    public class EpisodeInfoResponse
+    {
+        public string target { get; set; }
+        public string type { get; set; }
+        public string name { get; set; }
+    }
+}
