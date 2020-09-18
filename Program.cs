8768a4e9e0c3bdbe3b82b46ad0089c66faf838ee
Devil7DK <dineshthangavel47@gmail.com>
2020-09-18 14:19:34 +0530

    Implement download queue and concurrent downloads

--- Program.cs
+++ Program.cs
@@ -1,8 +1,11 @@
 ﻿using Devil7.Utils.Automation.NineAnimeDownloader.Models;
 using Devil7.Utils.Automation.NineAnimeDownloader.Utils;
+using ShellProgressBar;
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Text.RegularExpressions;
+using System.Threading.Tasks;
 
 namespace Devil7.Utils.Automation.NineAnimeDownloader
 {
@@ -10,7 +13,18 @@ namespace Devil7.Utils.Automation.NineAnimeDownloader
     {
         private static string AnimeId = "";
         private static Server SelectedServer = null;
-        private static string[] EpisodesToDownload = null;
+        private static string[] EpisodesToDownloadArray = null;
+
+        private static int ConcurrentJobs = 1;
+        private static readonly Queue<Episode> EpisodesToDownload = new Queue<Episode>();
+        private static readonly List<DownloadJob> DownloadJobs = new List<DownloadJob>();
+
+        private static ProgressBar MainProgressBar;
+
+        public static int TotalDownloads = 0;
+        public static int CompletedDownloads = 0;
+        public static int FailedDownloads = 0;
+        public static List<string> ErrorMessages = new List<string>();
 
         static void Main(string[] args)
         {
@@ -93,7 +107,7 @@ namespace Devil7.Utils.Automation.NineAnimeDownloader
                     }
                     else
                     {
-                        EpisodesToDownload = episodesToDownload;
+                        EpisodesToDownloadArray = episodesToDownload;
                         break;
                     }
                 }
@@ -108,8 +122,8 @@ namespace Devil7.Utils.Automation.NineAnimeDownloader
             while (true)
             {
                 Console.WriteLine("Selected Server: {0} ({1} Episode{2})", SelectedServer.Name, SelectedServer.Episodes.Count, SelectedServer.Episodes.Count > 1 ? "s" : "");
-                Console.WriteLine("Selcted Episodes:\n{0}", Common.ArrayToRangeString(EpisodesToDownload));
-                Console.Write("\nDo you want to continue downloading above {0} episodes? [Y/n]? ", EpisodesToDownload.Length);
+                Console.WriteLine("Selcted Episodes:\n{0}", Common.ArrayToRangeString(EpisodesToDownloadArray));
+                Console.Write("\nDo you want to continue downloading above {0} episodes? [Y/n]? ", EpisodesToDownloadArray.Length);
                 ConsoleKeyInfo keyInfo = Console.ReadKey();
 
                 if (keyInfo.Key == ConsoleKey.Y || keyInfo.Key == ConsoleKey.Enter)
@@ -123,8 +137,113 @@ namespace Devil7.Utils.Automation.NineAnimeDownloader
             }
 
             Console.Clear();
+            while (true)
+            {
+                try
+                {
+                    Console.Write("Enter Number of Concurrent Jobs:");
+                    ConcurrentJobs = int.Parse(Console.ReadLine());
+
+                    if (ConcurrentJobs < 1)
+                        ConcurrentJobs = 1;
+
+                    break;
+                }
+                catch (Exception ex)
+                {
+                    Console.Error.Write("Invalid Input! ");
+                    TryAgainOrQuit();
+                }
+            }
+
+            foreach (string episodeName in EpisodesToDownloadArray)
+            {
+                Episode episode = SelectedServer.Episodes.Find(episode => episode.Name == episodeName);
+                if (episode != null)
+                    EpisodesToDownload.Enqueue(episode);
+            }
+
+            TotalDownloads = EpisodesToDownload.Count;
+
+            ProgressBarOptions progressBarOptions = new ProgressBarOptions()
+            {
+                ForegroundColor = ConsoleColor.Green,
+                ProgressBarOnBottom = true
+            };
+            MainProgressBar = new ProgressBar((int)(TotalDownloads * 100), string.Format("Downloading {0} Episodes...", TotalDownloads), progressBarOptions);
+
+            for (int i = 0; i < ConcurrentJobs; i++)
+            {
+                AddJob();
+            }
+
+            System.Timers.Timer progressUpdateTimer = new System.Timers.Timer(500)
+            {
+                AutoReset = true,
+            };
+            progressUpdateTimer.Elapsed += ProgressUpdateTimer_Elapsed;
+            progressUpdateTimer.Start();
+
+            IEnumerable<Task> Tasks = GetIncompleteTasks();
+            while (Tasks.Count() > 0)
+            {
+                Task.WhenAll(Tasks).Wait();
+                Tasks = GetIncompleteTasks();
+            }
+
+            Console.Clear();
+            Console.WriteLine("=============================================");
+            Console.WriteLine("             All Tasks Complted!             ");
+            Console.WriteLine("=============================================");
+            Console.WriteLine(" - Total Downloads\t: {0}", TotalDownloads);
+            Console.WriteLine(" - Completed Downloads\t: {0}", CompletedDownloads);
+            Console.WriteLine(" - Failed Downloads\t: {0}", FailedDownloads);
+            if (ErrorMessages.Count > 0)
+            {
+                Console.Error.WriteLine(" - Error Messages\t: {0}", ErrorMessages[0]);
+                for (var i = 1; i < ErrorMessages.Count; i++)
+                    Console.Error.WriteLine("\t\t\t  * {0}", ErrorMessages[i]);
+            }
+            Console.WriteLine("=============================================");
+
+            Console.Clear();
+            NineAnime.GetVideoUrl(SelectedServer.Id, SelectedServer.Episodes.Find(episode => episode.Name == EpisodesToDownloadArray[0]).Id).Wait();
+
 
             Console.ReadLine();
+        }
+
+        private static void ProgressUpdateTimer_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            MainProgressBar.Tick(DownloadJobs.Sum((job) => job.Progress), string.Format("Downloaded {0} of {1}. {2} Failed!", CompletedDownloads, TotalDownloads, FailedDownloads));
+        }
+
+        private static IEnumerable<Task> GetIncompleteTasks()
+        {
+            return DownloadJobs.Select((job) => job.DownloadTask).Where((task) => !task.IsCompleted);
+        }
+
+        private static void AddJob()
+        {
+            if (EpisodesToDownload.Count > 0)
+            {
+                Episode episode = EpisodesToDownload.Dequeue();
+
+                string downloadUrl = NineAnime.GetVideoUrl(SelectedServer.Id, episode.Id).Result;
+
+                if (string.IsNullOrEmpty(downloadUrl))
+                {
+                    ErrorMessages.Add(string.Format("Episode {0}: Failed to Get Download URL!", episode.Name));
+                    AddJob();
+                }
+                else
+                {
+                    DownloadJob job = new DownloadJob(episode.Name, downloadUrl, MainProgressBar);
+                    job.Start();
+                    DownloadJobs.Add(job);
+                    job.DownloadTask.ContinueWith((Task task) => AddJob());
+                }
+            }
         }
 
         static void TryAgainOrQuit()
